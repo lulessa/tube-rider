97502d331ed01733fcd3cfe24d350986e2ce7272
Lucas Lessa <lucas@skyverge.com>
2017-11-25 20:36:17 -1000

    Wave model with seeded waves, output in PipelinesController

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171126063206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "waves", force: :cascade do |t|
+    t.integer "face_height"
+    t.string "tube_shape"
+    t.boolean "exit_available"
+    t.boolean "exit_clean"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
